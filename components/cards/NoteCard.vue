e96967e7f45b1328c80f2c734c3bd4da2763a292
zhrui001 <2443505970@qq.com>
2025-07-03 19:35:14 +0800

    update card height

--- components/cards/NoteCard.vue
+++ components/cards/NoteCard.vue
@@ -122,7 +122,7 @@ function openDetails() {
 }
 .note-card {
   width: 45vw;
-  height: calc((100vh - 96px) / 3);
+  height: 28vh;
   background-color: #ffff;
   border-radius: 12px;
   box-shadow: 0 4px 8px rgba(0,0,0,0.1);
